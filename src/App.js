e5f66607d59f96acb9ba73973f26c861a5cbd404
unknown <skawldhks@naver.com>
2023-04-04 16:08:16 +0900

    2번째

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
     <div>
     <div>Hello React</div>
     <h1>안녕하세요</h1>
+    <h2 style="background:black">처음 만들어봐요</h2>
     </div>
   );
 }
 
-export default App;
+export default App
