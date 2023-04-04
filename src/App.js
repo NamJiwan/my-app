f63e8d0a63291086007fb4c06c7d2c0b407d9f9e
unknown <skawldhks@naver.com>
2023-04-04 16:11:40 +0900

    2번째입니다

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
     <div>
     <div>Hello React</div>
     <h1>안녕하세요</h1>
-    <h2 style="background:black">처음 만들어봐요</h2>
+    <h2>처음 만들어봐요</h2>
+    <h3>처음 만들어봐요</h3>
     </div>
   );
 }
