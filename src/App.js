cff79e20871878083c1185a348d18d80709942b0
unknown <skawldhks@naver.com>
2023-04-04 16:23:20 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
 function App() {
   return (
     <div>
-    <div>Hello React</div>
-    <h1>안녕하세요</h1>
-    <h2>처음 만들어봐요</h2>
     <h3>처음 만들어봐요</h3>
+    <h3>처음 만들어봐요</h3>
+    
     </div>
   );
 }
